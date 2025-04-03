7d74eed34d7f816910b2be6925d4602e4d8dde3f
ISPL <tatsam.shrestha@info.com.np>
2025-04-03 11:52:44 +0545

    Database Connection and Deploy in MOnsterAsp.net

--- TaskManagement/Program.cs
+++ TaskManagement/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.TaskManagement.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+//I have used Connection From Appsetting
+builder.Services.AddDbContext<ApplicationDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
